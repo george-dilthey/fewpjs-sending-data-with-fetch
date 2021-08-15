c7d814016f5ed0d8a1b0c9e3bac6eb96a32782e7
George Dilthey <george.dilthey@gmail.com>
2021-08-15 15:37:04 -0400

    commit tree 9a01a8a91919f01df632fe18a12693805ddec2f3

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+
+function submitData(name, email){
+    const formData = {
+        name: name,
+        email: email
+    };
+    
+    const configObj = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(object => append(object.id))
+        .catch(error => {
+            append(error)
+          });
+}
+
+function append(content){
+    let div = document.createElement('div')
+    div.innerHTML = content
+    document.querySelector('div.ids').appendChild(div)
+}
